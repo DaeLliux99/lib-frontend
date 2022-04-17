f89814d5da41bb8c71ef7ca611cfad54a84373b4
DaeLliux99 <dino_esteban@hotmail.com>
2022-04-17 03:21:14 -0500

    initialprops

--- pages/documento/[id].js
+++ pages/documento/[id].js
@@ -3,15 +3,22 @@ import PanelDocumento from '../../components/PanelDocumento';
 import Footer from '../../components/Footer';
 import { useRouter } from 'next/router'
 import useObtenerArticulo from '../../hooks/articulosHooks';
+import axios from 'axios';
 
-const Doc = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  console.log(id);
-  const { articulo, cargando } = useObtenerArticulo(id);
-  console.log(cargando)
+const Doc = ( {articulo} ) => {
+  //const router = useRouter();
+  //const { id } = router.query;
+  //console.log(id);
+  //const { articulo, cargando } = useObtenerArticulo(id);
+  //console.log(cargando)
   return (
-    <div className="home">
+    <div>
+      <Header />
+      <PanelDocumento articulo={articulo} />
+      <Footer />
+    </div>
+
+    /*<div className="home">
       {!cargando ? (
         <div>
           <Header />
@@ -21,8 +28,35 @@ const Doc = () => {
       ) : (
         <div>Cargando</div>
       )}
-    </div>
+    </div>*/
   );
 };
+
+export const getStaticPaths = async () => {
+  const { data } = await axios.get("https://edu-repo.azurewebsites.net/api/articulos");
+  //console.log(data);
+  const paths = data.map(articulo => {
+    return {
+      params: { id: articulo.idArticulo.toString() }
+    }
+  });
+
+  //console.log(paths);
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const { data } = await axios.get(`https://edu-repo.azurewebsites.net/api/articulos/${id}`);
+  //console.log(data);
+  return {
+    props: {
+      articulo: data
+    }
+  }
+}
 
 export default Doc;
